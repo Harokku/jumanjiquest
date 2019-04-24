c85ae37babc1052e5633adc9c3cc6346cfbc3695
harokku <crenna.simone@gmail.com>
2019-04-24 12:53:51 +0200

    Quest done

--- src/Squad.js
+++ src/Squad.js
@@ -5,32 +5,39 @@ import Quest from "./Quest";
 const Squad = ({match}) => {
   const storageName = 'answeredQuest'
   const squad = match.params.squadName
-  const [actualQuest, setActualQuest] = useState(0)
-  const [answeredQuest, setAnsweredQuest] = useState(localStorage[storageName] || [])
+  const [answeredQuest, setAnsweredQuest] = useState( [])
+  const [actualQuest, setActualQuest] = useState(Math.floor(Math.random() * 8))
 
   const questlist = [
     {id: 0, qr: 'ghepardo', password: 'ghepardo'},
     {id: 1, qr: 'lince', password: 'lince'},
     {id: 2, qr: 'serpente', password: 'serpente'},
-    {id: 3, qr: 'serpente', password: 'serpente'},
-    {id: 4, qr: 'orso', password: 'orso'},
-    {id: 5, qr: 'lupo', password: 'lupo'},
-    {id: 6, qr: 'piovra', password: 'piovra'},
-    {id: 7, qr: 'coccodrillo', password: 'coccodrillo'},
-    {id: 8, qr: 'gufo', password: 'gufo'},
+    {id: 3, qr: 'orso', password: 'orso'},
+    {id: 4, qr: 'lupo', password: 'lupo'},
+    {id: 5, qr: 'piovra', password: 'piovra'},
+    {id: 6, qr: 'coccodrillo', password: 'coccodrillo'},
+    {id: 7, qr: 'gufo', password: 'gufo'},
   ];
+  const [questToAnswer, setQuestToAnswer] = useState(questlist)
 
   const handleQuestAnswer = (questId) => {
-    const answeredList = [...answeredQuest, questId]
-    setAnsweredQuest(answeredList)
+    const answeredList = [...answeredQuest, questId];
+    const newQuestList = questToAnswer.filter(function( obj ) {
+      return obj.id !== questId;
+    });
+    setQuestToAnswer(newQuestList)
+
+    setAnsweredQuest(answeredList);
     localStorage.setItem(storageName, answeredList)
-  }
+    setActualQuest(Math.floor(Math.random() * (questToAnswer.length - 1)))
+  };
 
   return (
     <>
-      {console.log(squad)}
+      {console.log(actualQuest)}
       {console.info(answeredQuest)}
-      {answeredQuest.length >= 9
+      {console.log(questToAnswer)}
+      {answeredQuest.length >= 8
         ? <div className="message is-success">
           <div className="message-header">
             <p>Complimenti! Gioco finito!</p>
@@ -42,9 +49,9 @@ const Squad = ({match}) => {
             il gioco e' fatto<br/>
           </div>
         </div>
-        : <Quest id={actualQuest}
-                 qr={questlist[actualQuest].qr}
-                 password={questlist[actualQuest].password}
+        : <Quest id={questToAnswer[actualQuest].id}
+                 qr={questToAnswer[actualQuest].qr}
+                 password={questToAnswer[actualQuest].password}
                  answerQuest={handleQuestAnswer}
         />
       }
